995507c9b6a35df0210ac9dab1977110026f9ce1
Tales Araujo <talesaraujodossantos@gmail.com>
2021-12-08 12:24:10 -0300

    Added laplacian and high-boost filterings

--- backend/image_processor/core/filtering.py
+++ backend/image_processor/core/filtering.py
@@ -5,6 +5,7 @@
 # Average filtering 
 
 import numpy as np
+from core import kernels, intensity
 
 
 def convolve2D(img: np.ndarray, filter: np.ndarray=None, strides: int=1) -> np.ndarray:
@@ -48,3 +49,16 @@ def convolve2D(img: np.ndarray, filter: np.ndarray=None, strides: int=1) -> np.n
                     break
 
     return output
+
+
+def laplacian(img: np.ndarray, rescale=True) -> np.ndarray:
+    img_l = convolve2D(img, kernels.LAPLACIAN)
+    return intensity.rescale(img_l) if rescale else img_l
+
+
+def high_boost(img: np.ndarray) -> np.ndarray:
+    img_g = convolve2D(img, kernels.GAUSSIAN_BLUR_5x5)
+    img_b = img - img_g
+    img_p = img + img_b
+
+    return img_p
